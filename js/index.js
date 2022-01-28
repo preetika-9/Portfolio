22df65ae23eea448f14f84f70da031fa7fd1aba1
preetika-9 <preetikapandey71@gmail.com>
2022-01-28 11:10:55 +0545

    scroll to top btn added

--- js/index.js
+++ js/index.js
@@ -31,3 +31,18 @@ var options = {
 var typed = new Typed('.typing', options);
 
 
+var mybutton = document.getElementById("myBtn");
+window.onscroll = function() {scrollFunction()};
+function scrollFunction(){
+    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20){
+        mybutton.style.display = "block";
+    }else{
+        mybutton.style.display = "none";
+    }
+}
+
+function topFunction(){
+    document.body.scrollTop = 0;
+    document.documentElement.scrollTop = 0;
+}
+
